d93c0f951adf08a2e849ffb4ed9cc47f459cac75
aleksandr_help@mail.ru <aleksandr_help@mail.ru>
2023-02-18 10:31:40 +0700

    Добавьте файлы проекта.

---
+++ Lab1_2023/VM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_2023
+{
+    internal class VM
+    {
+        FileStream file;
+        Page[] pages = new Page[3];
+        public VM()
+        {
+            file = File.Create("123");
+            file.WriteByte();
+        }
+    }
+}
